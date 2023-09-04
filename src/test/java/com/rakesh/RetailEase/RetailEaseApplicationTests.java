e63be65bff01764ca3ff8e30cf2cf26de6b84b11
rak626 <rakeshacot@gmail.com>
2023-09-04 11:59:35 +0530

    first project setup commit

---
+++ src/test/java/com/rakesh/RetailEase/RetailEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakesh.RetailEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
