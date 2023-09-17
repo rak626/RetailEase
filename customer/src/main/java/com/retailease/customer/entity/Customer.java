e6a1f5c4bba8711221df899e8f69443f5a780d15
rak626 <rakeshacot@gmail.com>
2023-09-17 10:25:28 +0530

    Customer module added

---
+++ customer/src/main/java/com/retailease/customer/entity/Customer.java
@@ -0,0 +1,39 @@
+package com.retailease.customer.entity;
+
+import lombok.*;
+import org.apache.tomcat.jni.Address;
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer_id")
+    Long customerId;
+    @Column(name = "firstname")
+    String firstName;
+    @Column(name = "lastname")
+    String lastName;
+    @Column(name = "email")
+    String email;
+    @Column(name = "phone")
+    String phone;
+    /*@Column(name = "address")
+    String address;
+    @Column(name = "username")
+    String username;
+    @Column(name = "password")
+    String password;*/
+    @Column(name = "registration_date")
+    Date registrationDate;
+}
---
+++ customer/src/main/java/com/retailease/customer/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,131 @@
+package com.retailease.customer.service.impl;
+
+import com.retailease.customer.entity.Customer;
+import com.retailease.customer.exceptions.ResourceNotFoundException;
+import com.retailease.customer.payload.request.CustomerReqDto;
+import com.retailease.customer.payload.response.CustomerResDto;
+import com.retailease.customer.repository.CustomerRepo;
+import com.retailease.customer.service.CustomerService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    public CustomerServiceImpl(CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+    @Override
+    public CustomerResDto addNewCustomer(CustomerReqDto customerReqDto) {
+        Customer customer = Customer.builder()
+                .firstName(customerReqDto.getFirstName())
+                .lastName(customerReqDto.getLastName())
+                .email(customerReqDto.getEmail())
+                .phone(customerReqDto.getPhone())
+                .registrationDate(new Date())
+                .build();
+
+        Customer savedCustomer = customerRepo.save(customer);
+
+        return CustomerResDto.builder()
+                .customerId(savedCustomer.getCustomerId())
+                .firstName(savedCustomer.getFirstName())
+                .lastName(savedCustomer.getLastName())
+                .email(savedCustomer.getEmail())
+                .phone(savedCustomer.getPhone())
+                .registrationDate(savedCustomer.getRegistrationDate())
+                .build();
+    }
+
+    @Override
+    public void deleteCustomerById(Long customerId) {
+        Customer customer = customerRepo.findById(customerId).orElseThrow(() -> new ResourceNotFoundException("Customer", "Id", customerId));
+        customerRepo.delete(customer);
+    }
+
+    @Override
+    public CustomerResDto updateCustomer(Long customerId, CustomerReqDto customerReqDto) {
+        Customer customer = customerRepo.findById(customerId).orElseThrow(() -> new ResourceNotFoundException("Customer", "Id", customerId));
+
+        if (customerReqDto.getFirstName() != null) customer.setFirstName(customerReqDto.getFirstName());
+        if (customerReqDto.getLastName() != null) customer.setLastName(customerReqDto.getLastName());
+        if (customerReqDto.getEmail() != null) customer.setEmail(customerReqDto.getEmail());
+        if (customerReqDto.getPhone() != null) customer.setPhone(customerReqDto.getPhone());
+        Customer savedCustomer = customerRepo.save(customer);
+
+        return CustomerResDto.builder()
+                .customerId(savedCustomer.getCustomerId())
+                .firstName(savedCustomer.getFirstName())
+                .lastName(savedCustomer.getLastName())
+                .email(savedCustomer.getEmail())
+                .phone(savedCustomer.getPhone())
+                .registrationDate(savedCustomer.getRegistrationDate())
+                .build();
+    }
+
+    @Override
+    public List<CustomerResDto> findCustomerByType(String type, String value) {
+        List<Customer> customerList = new ArrayList<>();
+        if (type.equalsIgnoreCase("firstname")) {
+            List<Optional<Customer>> foundCustomerList = customerRepo.findAllByFirstName(value);
+            customerList = foundCustomerList.stream().map(customer -> customer.orElse(null)).collect(Collectors.toList());
+        } else if (type.equalsIgnoreCase("lastname")) {
+            List<Optional<Customer>> foundCustomerList = customerRepo.findAllByLastName(value);
+            customerList = foundCustomerList.stream().map(customer -> customer.orElse(null)).collect(Collectors.toList());
+        } else if (type.equalsIgnoreCase("email")) {
+            List<Optional<Customer>> foundCustomerList = customerRepo.findAllByEmail(value);
+            customerList = foundCustomerList.stream().map(customer -> customer.orElse(null)).collect(Collectors.toList());
+        } else if (type.equalsIgnoreCase("phone")) {
+            List<Optional<Customer>> foundCustomerList = customerRepo.findAllByPhone(value);
+            customerList = foundCustomerList.stream().map(customer -> customer.orElse(null)).collect(Collectors.toList());
+        }
+        return customerList.stream().map(customer -> {
+            return CustomerResDto.builder()
+                    .customerId(customer.getCustomerId())
+                    .firstName(customer.getFirstName())
+                    .lastName(customer.getLastName())
+                    .email(customer.getEmail())
+                    .phone(customer.getPhone())
+                    .registrationDate(customer.getRegistrationDate())
+                    .build();
+
+        }).collect(Collectors.toList());
+    }
+
+    @Override
+    public CustomerResDto findCustomerById(Long customerId) {
+        Customer customer = customerRepo.findById(customerId).orElseThrow(() -> new ResourceNotFoundException("Customer", "id", customerId));
+        return CustomerResDto.builder()
+                .customerId(customer.getCustomerId())
+                .firstName(customer.getFirstName())
+                .lastName(customer.getLastName())
+                .email(customer.getEmail())
+                .phone(customer.getPhone())
+                .registrationDate(customer.getRegistrationDate())
+                .build();
+    }
+
+    @Override
+    public List<CustomerResDto> findAllCustomer() {
+        List<Customer> allCustomers = customerRepo.findAll();
+
+        return allCustomers.stream().map(customer -> CustomerResDto.builder()
+                        .customerId(customer.getCustomerId())
+                        .firstName(customer.getFirstName())
+                        .lastName(customer.getLastName())
+                        .email(customer.getEmail())
+                        .phone(customer.getPhone())
+                        .registrationDate(customer.getRegistrationDate())
+                        .build()
+                )
+                .collect(Collectors.toList());
+    }
+}
